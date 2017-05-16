99983516a2d9a8f336598a84125e82590e1388bb
DJ_MacHack <mail@hendrik-haas.de>
2017-05-16 15:17:32 +0200

    1st try

---
+++ ArrayTools.h
@@ -0,0 +1,12 @@
+/**
+* @file: ArrayTools.h
+* @author:DJ MacHack
+* @date: 16.05.2017.
+* Matrnr.: 3747719
+* mail@hendrik-haas.de
+*/
+
+#ifndef INC_3_ARRAYTOOLS_H
+#define INC_3_ARRAYTOOLS_H
+
+#endif //INC_3_ARRAYTOOLS_H
