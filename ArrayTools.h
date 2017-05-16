cb15ed2865a3c14da992fc7ac75782ddc273e5aa
DJ_MacHack <mail@hendrik-haas.de>
2017-05-16 15:34:35 +0200

    whitespace fixed + comments

--- ArrayTools.h
+++ ArrayTools.h
@@ -4,6 +4,7 @@
 * @date: 16.05.2017.
 * Matrnr.: 3747719
 * mail@hendrik-haas.de
+* Partner: Julian Bruna
 */
 
 #ifndef INC_3_ARRAYTOOLS_H
@@ -15,6 +16,13 @@
 
 using namespace std;
 
+/**
+ * You can read in your array via cin.
+ * @tparam T
+ * @param array
+ * @param size
+ * @return
+ */
 template<typename T>
 T readArray(T array, size_t size){
     cout << "Bitte " << size << " Werte eingeben:" << endl;
@@ -24,15 +32,29 @@ T readArray(T array, size_t size){
     }
 }
 
+/**
+ * Prints your array
+ * @tparam T
+ * @param array
+ * @param size
+ * @return
+ */
 template<typename T>
 T printArray(T array, size_t size){
     cout << "[ ";
     for(size_t i = 0; i < size-1; i++){
-        cout << array[i] << " ,";
+        cout << array[i] << ", ";
     }
     cout << array[size-1] << " ]" << endl;
 }
 
+/**
+ * Finds the max value of your array.
+ * @tparam T
+ * @param array
+ * @param size
+ * @return
+ */
 template<typename T>
 T findMax(T* array, size_t size){
     T max = array[0];
@@ -44,6 +66,14 @@ T findMax(T* array, size_t size){
     return max;
 }
 
+/**
+ * Optimized bubble sort
+ * https://en.wikipedia.org/wiki/Bubble_sort#Optimizing_bubble_sort
+ * @tparam T
+ * @param array
+ * @param size
+ * @return
+ */
 template<typename T>
 T bubbleSort(T array, size_t size) {
     size_t sizec = size;
@@ -59,6 +89,13 @@ T bubbleSort(T array, size_t size) {
     }
 }
 
+/**
+ * Opens all functions to test the array template
+ * @tparam T
+ * @param array 
+ * @param size
+ * @return
+ */
 template<typename T>
 T testeArray(T* array, size_t size) {
     readArray(array, size);
